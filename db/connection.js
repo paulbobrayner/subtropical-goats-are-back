a4485e910a426820a730e8839569d4e3e0f09fa4
Paul <paulrobertrayner@hotmail.co.uk>
2021-10-18 07:43:13 +0100

    update conection

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,13 @@
 const ENV = process.env.NODE_ENV || 'development';
 const config =
   ENV === 'production'
-    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    ? {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
     : require('../knexfile');
 
 module.exports = require('knex')(config);
